6c35b02923f1310f90743e40b5a05c5f97580be0
WENGzhang <wengzhang@WENGzhangs-MacBook-Pro.local>
2018-10-31 13:30:23 +0100

    [ft_strcmp] No null crash

--- srcs/ft_stdlib/ft_strcmp.c
+++ srcs/ft_stdlib/ft_strcmp.c
@@ -14,6 +14,12 @@
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
+    if (!s1 && !s2)
+        return (0);
+    if (!s1)
+        return (-1);
+    if (!s2)
+        return (1);
 	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
